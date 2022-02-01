6046271a73eda6969a7b1b4ea39e5a25b09b48b5
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-02-01 23:43:20 +0300

    add calculatePalindrome method

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-public class Main {
+class Main {
     public static void main(String[] args) {
-        
+
+        System.out.println(calculatePalindrome(1005));
+    }
+
+    static long calculatePalindrome(long num) {
+        String numToCheck = String.valueOf(num);
+        StringBuilder reverseNum = new StringBuilder();
+
+        for (int i = numToCheck.length() - 1; i >= 0; i--) {
+            reverseNum.append(numToCheck.charAt(i));
+        }
+
+        return Long.parseLong(reverseNum.toString());
     }
 }
